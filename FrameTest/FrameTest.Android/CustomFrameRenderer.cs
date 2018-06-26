682012399017ac6c6a912209b7e6a1fb1e4e7e24
Gyllion van Elderen <gyllion.van.elderen@appstones.nl>
2018-06-26 10:45:08 +0200

    Add project files.

---
+++ FrameTest/FrameTest.Android/CustomFrameRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using FrameTest.Droid;
+using Xamarin.Forms;
+
+//[assembly: ExportRenderer(typeof(Frame), typeof(CustomFrameRenderer))]
+namespace FrameTest.Droid
+{
+    public class CustomFrameRenderer : Xamarin.Forms.Platform.Android.FrameRenderer
+    {
+        public CustomFrameRenderer(Context context) : base(context) { } 
+    }
+}
